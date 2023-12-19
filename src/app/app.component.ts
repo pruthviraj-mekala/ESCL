1f9bd59b688e7e2304f2ea1a57c0f19140710ddd
pmekala2 <pruthviraj.mekala@its.ny.gov>
2023-12-19 16:30:35 -0500

    integrating with Angular material and Angular core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,14 @@ import { FooterComponent } from './footer/footer.component';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewsBulletinComponent } from './news-bulletin/news-bulletin.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel/image-carousel.component';
+import { AlertModule, CarouselModule } from '@coreui/angular';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -15,13 +23,22 @@ import { NewsBulletinComponent } from './news-bulletin/news-bulletin.component';
     NavbarComponent,
     FooterComponent,
     ImageSliderComponent,
-    NewsBulletinComponent
+    NewsBulletinComponent,
+    ImageCarouselComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   AlertModule,
+   CarouselModule,
+   BrowserAnimationsModule,
+   MatToolbarModule, 
+   MatButtonModule, 
+   MatIconModule,
+   MatCardModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ provideAnimations() ],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
